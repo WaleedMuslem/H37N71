36992063faf51751df7a562639220f4e8506b3e3
waleedMuslem <waleedmosllm2020@gmail.com>
2023-05-18 14:46:15 +0200

    created write module

---
+++ modules/write/variables.tf
@@ -0,0 +1,24 @@
+variable "answer_1" {
+  type    = string
+  default = "default value for answer_1"
+}
+
+variable "answer_2" {
+  type    = string
+  default = "default value for answer_2"
+}
+
+variable "answer_3" {
+  type    = string
+  default = "default value for answer_3"
+}
+
+variable "answer_4" {
+  type    = string
+  default = "default value for answer_4"
+}
+
+variable "answer_5" {
+  type    = string
+  default = "default value for answer_5"
+}
